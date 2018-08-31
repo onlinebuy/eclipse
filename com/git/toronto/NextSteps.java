68c59743ba174ea299493bfd7d36c352797e49be
Priyesh  Shrivastava <Priyesh  Shrivastava@192.168.2.15>
2018-08-31 15:56:22 -0400

    commit#2 - 8/31

---
+++ com/git/toronto/NextSteps.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Priyesh  Shrivastava
+ *
+ */
+public class NextSteps {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Now that the connection is set up, lets commit");
+	}
+
+}
